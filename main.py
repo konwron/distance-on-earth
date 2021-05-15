b58309d3079fdeb933a2c7585aacecf408a06496
konwron <wronski.k97@gmail.com>
2021-05-15 09:43:33 +0200

    add inputs and modify output

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from geopy.geocoders import Nominatim       # needed for finding geo-coordinates
 from math import *                          # needed for measuring distance
+# from tkinter import *                       # GUI
+# from tkinter import filedialog, constants, messagebox
+
 
 deg_to_rad = float(pi / 180.0)
 radius = 6371       # 6371 km OR 3959 miles according to wikipedia
-# !!! zamienić na wartość odpowiadającą zaznaczonemu radio buttonowi !!!
+
+loc1_input = input("Wpisz pierwszą lokalizację: ")
+loc2_input = input("Wpisz drugą lokalizację: ")
 
 geolocator = Nominatim(user_agent="distance-on-earth")
-location1 = geolocator.geocode("Rzeszów")       # find 1st location
-location2 = geolocator.geocode("Kraków")        # find 2nd location
-# !!! zamienić stringi na wartości inputów !!!
+location1 = geolocator.geocode(loc1_input)      # find 1st location
+location2 = geolocator.geocode(loc2_input)      # find 2nd location
 
 
 # haversine formula
@@ -30,4 +34,44 @@ def haversine_formula(loc1, loc2):
     return radius * c
 
 
-print(haversine_formula(location1, location2))
+# tkinter configuration
+
+
+# window = Tk()       # tworzenie okna głównego programu
+#
+# window.geometry("600x400")      # rozmiar okna programu
+# window.columnconfigure(0, weight=1)
+# window.rowconfigure(0, weight=1)
+# window.title("Oblicz odległość pomiędzy dwoma punktami na kuli ziemskiej")      # ustawienie tytułu okna
+#
+#
+# location1_input = StringVar()
+# location1_description = Label(window, text="Podaj pierwszą lokalizację").pack()
+# location1_entry = Entry(window, width=40)
+# location1_entry.pack()     # Miejsce do wprowadzenia nazwy pierwszego miasta
+#
+#
+# location2_input = StringVar()
+# location2_description = Label(window, text="Podaj drugą lokalizację").pack()
+# location2_entry = Entry(window, width=40)
+# location2_entry.pack()     # Miejsce do wprowadzenia nazwy drugiego miasta
+#
+#
+# rb_var = StringVar()  # zmienna sterująca zaznaczeniem kontrolki
+# rb_kilometres = Radiobutton(window, variable=rb_var, value=6371, text="kilometry")        # kontrolka pierwsza
+# rb_miles = Radiobutton(window, variable=rb_var, value=3959, text="mile")
+# rb_var.set("km")    # ustawiam zaznaczenie na kontrolkę dla opcji kilometres
+# rb_kilometres.pack()
+# rb_miles.pack()     # Radiobutton z wyborem miar długości (km/miles)
+#
+# lokalizacja = Button(window, text="Oblicz", width=20, command=haversine_formula)
+# lokalizacja.pack()   # Wynik: w zaleźności od wyboru km lub mil wypisana będzie dana wartość adekwatna do miary długości
+#
+# window.bind('<Return>', haversine_formula)
+#
+# window.mainloop()       # wywołanie głównej pętli programu
+
+
+# print(haversine_formula(location1, location2))
+
+print("Odlegość między tymi miejscami wynosi: {} km".format(haversine_formula(location1, location2)))
