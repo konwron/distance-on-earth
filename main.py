ed219d73683b28d2586a8d534720eb7409f0bbd9
konwron <wronski.k97@gmail.com>
2021-05-30 19:32:04 +0200

    calculate distance based on radiobutton value

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from math import *                          # biblioteka potrzebna do obliczania
 from tkinter import *                       # GUI
 
 
-def main(loc1_name, loc2_name):
+def main(loc1_name, loc2_name, unit):
     geolocator = Nominatim(user_agent="distance-on-earth")  # wybór API do wyszukania miejsc - Nominatim (OpenStreetMap)
-    loc1 = geolocator.geocode(loc1_name.get())  # szukanie współrzędnych 1 miejsca
-    loc2 = geolocator.geocode(loc2_name.get())  # szukanie współrzędnych 2 miejsca
-    radius = 6371  # promień: 6371 km lub 3959 mil (Wikipedia)
+    loc1 = geolocator.geocode(loc1_name)  # szukanie współrzędnych 1 miejsca
+    loc2 = geolocator.geocode(loc2_name)  # szukanie współrzędnych 2 miejsca
+
+    radius = 3958.8 if unit == "mi" else 6371   # promień Ziemi: 6371 km lub 3958.8 mi (Wikipedia)
 
     result = haversine_formula(loc1, loc2, radius)
 
-    print("Odległość: {}".format(result))
+    print("Odległość między {} i {} wynosi {} {}.\n".format(loc1_name, loc2_name, result, unit))
 
 
 # Metoda Haversine'a
-def haversine_formula(loc1, loc2, rad):
+def haversine_formula(loc1, loc2, radius):
 
     lat1 = loc1.latitude  # szerokość geograficzna 1 miejsca
     lon1 = loc1.longitude  # długość geograficzna 1 miejsca
@@ -33,7 +34,7 @@ def haversine_formula(loc1, loc2, rad):
 
     c = round(2 * atan2(sqrt(a), sqrt(1 - a)), 3)  # wynik zaokrąglony do 3 miejsc po przecinku
 
-    return rad * c
+    return radius * c
 
 
 # tworzenie GUI (interfejsu użytkownika)
@@ -48,26 +49,26 @@ loc1_description = Label(window, text="Podaj pierwszą lokalizację").pack()
 loc1_entry = Entry(window, width=40)
 loc1_entry.pack()     # miejsce do wprowadzenia nazwy pierwszego miasta
 
-
 loc2_input = StringVar()
 loc2_description = Label(window, text="Podaj drugą lokalizację").pack()
 loc2_entry = Entry(window, width=40)
 loc2_entry.pack()     # miejsce do wprowadzenia nazwy drugiego miasta
 
+radius_rb = StringVar()  # zmienna sterująca zaznaczeniem kontrolki
+rb_frame = LabelFrame(window, text='Wybierz jednostkę miary')
+rb_frame.pack()
+km_rb = Radiobutton(rb_frame, variable=radius_rb, value="km", indicator=0, text="kilometry")
+km_rb.pack(fill=X, ipady=5)
+mi_rb = Radiobutton(rb_frame, variable=radius_rb, value="mi", indicator=0, text="mile")
+mi_rb.pack(fill=X, ipady=5)
+radius_rb.set("km")    # ustawiam początkowe zaznaczenie na kontrolkę km
 
-# rb_var = StringVar()  # zmienna sterująca zaznaczeniem kontrolki
-# rb_kilometres = Radiobutton(window, variable=rb_var, value=6371, text="kilometry")        # kontrolka pierwsza
-# rb_miles = Radiobutton(window, variable=rb_var, value=3959, text="mile")
-# rb_var.set("km")    # ustawiam zaznaczenie na kontrolkę dla opcji kilometres
-# rb_kilometres.pack()
-# rb_miles.pack()     # Radiobutton z wyborem miar długości (km/miles)
+run = Button(window, text="Oblicz", width=20, command=lambda: main(loc1_entry.get(), loc2_entry.get(), radius_rb.get()))
+run.pack()   # wypisana będzie wartość adekwatna do miary długości
 
-lokalizacja = Button(window, text="Oblicz", width=20, command=lambda: main(loc1_entry, loc2_entry))
-lokalizacja.pack()   # wynik: wypisana będzie wartość adekwatna do miary długości
+window.bind('<Return>', lambda event: main(loc1_entry.get(), loc2_entry.get(), radius_rb.get()))
 
-window.bind('<Return>', lambda event: main(loc1_entry, loc2_entry))
-
-window.mainloop()       # wywołanie głównej pętli programu
+window.mainloop()       # wywołanie programu
 
 
 
