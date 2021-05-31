7444e7856e6d5d264563b6cedf05048d29a44347
konwron <wronski.k97@gmail.com>
2021-05-31 20:56:08 +0200

    handle wrong input error

--- main.py
+++ main.py
@@ -4,35 +4,42 @@ from tkinter import *                       # GUI
 
 
 def main(*args):    # główna funkcja programu z opcjonalnymi argumentami
+
     geolocator = Nominatim(user_agent='distance-on-earth')  # wybór API do wyszukania miejsc - Nominatim (OpenStreetMap)
     loc1 = geolocator.geocode(loc1_entry.get())  # szukanie współrzędnych 1 miejsca
     loc2 = geolocator.geocode(loc2_entry.get())  # szukanie współrzędnych 2 miejsca
+    try:
+        radius = 3958.8 if (radius_rb.get()) == 'mi' else 6371      # promień Ziemi: 6371 km lub 3958.8 mi (Wikipedia)
 
-    radius = 3958.8 if (radius_rb.get()) == 'mi' else 6371   # promień Ziemi: 6371 km lub 3958.8 mi (Wikipedia)
+        result = haversine_formula(loc1, loc2, radius)
 
-    result = haversine_formula(loc1, loc2, radius)
-
-    print('Odległość między {} i {} wynosi {} {}.\n'.format(loc1_entry.get(), loc2_entry.get(), result, radius_rb.get()))
+        print('Odległość między {} i {} wynosi {} {}.\n'.format(loc1_entry.get(), loc2_entry.get(), result, radius_rb.get()))
+    except AttributeError:
+        print(loc1, loc2)
+        if not loc1:
+            print('Podaj istniejącą lokalizację')
+        if not loc2:
+            print('Podaj istniejącą lokalizację')
 
 
 # Metoda Haversine'a
 def haversine_formula(loc1, loc2, radius):
 
-    lat1 = loc1.latitude  # szerokość geograficzna 1 miejsca
-    lon1 = loc1.longitude  # długość geograficzna 1 miejsca
+    lat1 = loc1.latitude        # szerokość geograficzna 1 miejsca
+    lon1 = loc1.longitude       # długość geograficzna 1 miejsca
 
-    lat2 = loc2.latitude  # szerokość geograficzna 2 miejsca
-    lon2 = loc2.longitude  # długość geograficzna 2 miejsca
+    lat2 = loc2.latitude        # szerokość geograficzna 2 miejsca
+    lon2 = loc2.longitude       # długość geograficzna 2 miejsca
 
     deg_to_rad = float(pi / 180.0)
 
-    lat_distance = (lat2 - lat1) * deg_to_rad  # zamiana stopni na radiany
+    lat_distance = (lat2 - lat1) * deg_to_rad       # zamiana stopni na radiany
     lon_distance = (lon2 - lon1) * deg_to_rad
 
     # obliczanie na podstawie wzoru (https://en.wikipedia.org/wiki/Haversine_formula)
     a = pow(sin(lat_distance / 2), 2) + cos(lat1 * deg_to_rad) * cos(lat2 * deg_to_rad) * pow(sin(lon_distance / 2), 2)
 
-    c = round(2 * atan2(sqrt(a), sqrt(1 - a)), 3)  # wynik zaokrąglony do 3 miejsc po przecinku
+    c = round(2 * atan2(sqrt(a), sqrt(1 - a)), 3)       # wynik zaokrąglony do 3 miejsc po przecinku
 
     return radius * c
 
@@ -73,16 +80,16 @@ run.pack()   # wypisana będzie wartość adekwatna do miary długości
 window.bind('<Return>', main)
 window.bind("<KeyPress-Escape>", Quit)
 
-window.mainloop()       # wywołanie programu
+window.mainloop()       # nasłuchiwanie zdarzeń z Tkintera
 
 # WERSJA UNIWERSALNA, Z UŻYCIEM LAMBDA (lambda jest potrzebna, by przekazać argumenty do wywoływanej funkcji)
 
 # def main(loc1_name, loc2_name, unit):
-#     geolocator = Nominatim(user_agent="distance-on-earth")  # wybór API do wyszukania miejsc - Nominatim (OpenStreetMap)
-#     loc1 = geolocator.geocode(loc1_name)  # szukanie współrzędnych 1 miejsca
-#     loc2 = geolocator.geocode(loc2_name)  # szukanie współrzędnych 2 miejsca
+#     geolocator = Nominatim(user_agent="distance-on-earth")
+#     loc1 = geolocator.geocode(loc1_name)
+#     loc2 = geolocator.geocode(loc2_name)
 #
-#     radius = 3958.8 if unit == "mi" else 6371   # promień Ziemi: 6371 km lub 3958.8 mi (Wikipedia)
+#     radius = 3958.8 if unit == "mi" else 6371
 #
 #     result = haversine_formula(loc1, loc2, radius)
 #
