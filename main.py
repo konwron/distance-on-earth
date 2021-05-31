c3079dddd705286265ad7d4ad73b860d100beca3
konwron <wronski.k97@gmail.com>
2021-05-31 20:12:02 +0200

    calling main w/o lambda

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from math import *                          # biblioteka potrzebna do obliczania
 from tkinter import *                       # GUI
 
 
-def main(loc1_name, loc2_name, unit):
-    geolocator = Nominatim(user_agent="distance-on-earth")  # wybór API do wyszukania miejsc - Nominatim (OpenStreetMap)
-    loc1 = geolocator.geocode(loc1_name)  # szukanie współrzędnych 1 miejsca
-    loc2 = geolocator.geocode(loc2_name)  # szukanie współrzędnych 2 miejsca
+def main(*args):    # główna funkcja programu z opcjonalnymi argumentami
+    geolocator = Nominatim(user_agent='distance-on-earth')  # wybór API do wyszukania miejsc - Nominatim (OpenStreetMap)
+    loc1 = geolocator.geocode(loc1_entry.get())  # szukanie współrzędnych 1 miejsca
+    loc2 = geolocator.geocode(loc2_entry.get())  # szukanie współrzędnych 2 miejsca
 
-    radius = 3958.8 if unit == "mi" else 6371   # promień Ziemi: 6371 km lub 3958.8 mi (Wikipedia)
+    radius = 3958.8 if (radius_rb.get()) == 'mi' else 6371   # promień Ziemi: 6371 km lub 3958.8 mi (Wikipedia)
 
     result = haversine_formula(loc1, loc2, radius)
 
-    print("Odległość między {} i {} wynosi {} {}.\n".format(loc1_name, loc2_name, result, unit))
+    print('Odległość między {} i {} wynosi {} {}.\n'.format(loc1_entry.get(), loc2_entry.get(), result, radius_rb.get()))
 
 
 # Metoda Haversine'a
@@ -37,36 +37,41 @@ def haversine_formula(loc1, loc2, radius):
     return radius * c
 
 
+def Quit(event):
+    quit()
+
+
 # tworzenie GUI (interfejsu użytkownika)
 window = Tk()       # tworzenie okna głównego programu
-window.geometry("600x400")      # rozmiar okna programu
+window.geometry('600x400')      # rozmiar okna programu
 window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1)
-window.title("Oblicz odległość pomiędzy dwoma punktami na kuli ziemskiej")      # ustawienie tytułu okna
+window.title('Oblicz odległość pomiędzy dwoma punktami na kuli ziemskiej')      # ustawienie tytułu okna
 
 loc1_input = StringVar()
-loc1_description = Label(window, text="Podaj pierwszą lokalizację").pack()
+loc1_description = Label(window, text='Podaj pierwszą lokalizację').pack()
 loc1_entry = Entry(window, width=40)
 loc1_entry.pack()     # miejsce do wprowadzenia nazwy pierwszego miasta
 
 loc2_input = StringVar()
-loc2_description = Label(window, text="Podaj drugą lokalizację").pack()
+loc2_description = Label(window, text='Podaj drugą lokalizację').pack()
 loc2_entry = Entry(window, width=40)
 loc2_entry.pack()     # miejsce do wprowadzenia nazwy drugiego miasta
 
 radius_rb = StringVar()  # zmienna sterująca zaznaczeniem kontrolki
 rb_frame = LabelFrame(window, text='Wybierz jednostkę miary')
 rb_frame.pack()
-km_rb = Radiobutton(rb_frame, variable=radius_rb, value="km", indicator=0, text="kilometry")
+km_rb = Radiobutton(rb_frame, variable=radius_rb, value='km', indicator=0, text='kilometry')
 km_rb.pack(fill=X, ipady=5)
-mi_rb = Radiobutton(rb_frame, variable=radius_rb, value="mi", indicator=0, text="mile")
+mi_rb = Radiobutton(rb_frame, variable=radius_rb, value='mi', indicator=0, text='mile')
 mi_rb.pack(fill=X, ipady=5)
-radius_rb.set("km")    # ustawiam początkowe zaznaczenie na kontrolkę km
+radius_rb.set('km')    # ustawiam początkowe zaznaczenie na kontrolkę km
 
-run = Button(window, text="Oblicz", width=20, command=lambda: main(loc1_entry.get(), loc2_entry.get(), radius_rb.get()))
+run = Button(window, text='Oblicz', width=20, command=main)
 run.pack()   # wypisana będzie wartość adekwatna do miary długości
 
-window.bind('<Return>', lambda event: main(loc1_entry.get(), loc2_entry.get(), radius_rb.get()))
+window.bind('<Return>', main)
+window.bind("<KeyPress-Escape>", Quit)
 
 window.mainloop()       # wywołanie programu
 
